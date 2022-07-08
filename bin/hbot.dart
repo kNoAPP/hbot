25301c23eea88c44ae8c4b0848d485ae7709004f
kNoAPP <alldoneb@gmail.com>
2022-07-08 12:43:40 -0700

    Initial commit

---
+++ bin/hbot.dart
@@ -0,0 +1,5 @@
+import 'package:hbot/hbot.dart' as hbot;
+
+void main(List<String> arguments) {
+  print('Hello world: ${hbot.calculate()}!');
+}
---
+++ test/hbot_test.dart
@@ -0,0 +1,8 @@
+import 'package:hbot/hbot.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
