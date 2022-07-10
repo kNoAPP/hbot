5e3b3cfb68bf9e24b8ac5313fe26907b036a1cd4
kNoAPP <alldoneb@gmail.com>
2022-07-10 09:53:32 -0700

    Add rich presence to the bot

--- lib/src/discord_bot.dart
+++ lib/src/discord_bot.dart
@@ -1,4 +1,6 @@
+import 'dart:async';
 import 'dart:io';
+import 'dart:math';
 
 import 'package:googleapis/customsearch/v1.dart';
 import 'package:googleapis_auth/googleapis_auth.dart';
@@ -12,33 +14,111 @@ Future<void> startBot({
   required String customSearchEngine,
   required File phrasesFile,
 }) async {
-  final imageApi = CustomSearchApi(clientViaApiKey(googleApiKey));
-
   final bot = NyxxFactory.createNyxxWebsocket(
       discordToken, GatewayIntents.allUnprivileged)
     ..registerPlugin(Logging())
     ..registerPlugin(CliIntegration())
-    ..registerPlugin(IgnoreExceptions())
-    ..connect();
+    ..registerPlugin(IgnoreExceptions());
 
-  final hBotPhraseGenerator = await PhraseGenerator.fromFile(phrasesFile);
+  await bot.connect();
+  // Let the bot's connection warm up. This isn't necessary, but it ensures
+  // future calls to Discord don't outright fail.
+  await Future.delayed(Duration(seconds: 2));
 
-  // Listen for message events
-  bot.eventsWs.onMessageReceived.listen((event) async {
-    if (event.message.content == '!h') {
-      await event.message.delete();
-      await event.message.channel
-          .sendMessage(MessageBuilder.content(await getImage(
-        imageApi: imageApi,
-        customSearchEngine: customSearchEngine,
-        query: 'Letter H',
-      )));
+  final phraseGenerator = await PhraseGenerator.fromFile(phrasesFile);
+  final imageApi = CustomSearchApi(clientViaApiKey(googleApiKey));
 
-      final phrase = hBotPhraseGenerator
-          .generatePhrase()
-          .replaceAll('%name%', '<@${event.message.author.id}>');
-      final message = '$phrase <@&421563234651471872>';
-      await event.message.channel.sendMessage(MessageBuilder.content(message));
-    }
+  await HDiscordBot(
+          bot: bot,
+          phraseGenerator: phraseGenerator,
+          imageApi: imageApi,
+          customSearchEngine: customSearchEngine)
+      .start();
+}
+
+class HDiscordBot {
+  HDiscordBot({
+    required this.bot,
+    required this.phraseGenerator,
+    required this.imageApi,
+    required this.customSearchEngine,
   });
+
+  INyxxWebsocket bot;
+  PhraseGenerator phraseGenerator;
+  CustomSearchApi imageApi;
+  String customSearchEngine;
+
+  final _rng = Random();
+
+  Future<void> start() async {
+    bot.eventsWs.onMessageReceived.listen((event) async {
+      if (event.message.content == '!h') {
+        await handleHMessage(event.message);
+      }
+    });
+
+    setRandomPresence();
+    Timer.periodic(Duration(hours: 2), (timer) => setRandomPresence());
+  }
+
+  final activities = [
+    ActivityBuilder.watching('your epic #plays'),
+    ActivityBuilder.watching('the US explode'),
+    ActivityBuilder.watching('California burn'),
+    ActivityBuilder.watching('Zucc watch you'),
+    ActivityBuilder.watching('Jere FC charts'),
+    ActivityBuilder.watching('James solo baron'),
+    ActivityBuilder.watching('Grant ward bushes'),
+    ActivityBuilder.watching('Josh play hentai'),
+    ActivityBuilder.watching('kNo gaslight people'),
+    ActivityBuilder.watching('DiamondFire'),
+    ActivityBuilder.watching('Arcane'),
+    ActivityBuilder.watching('puppers go woo'),
+    ActivityBuilder.watching('for H pings'),
+    ActivityBuilder.watching('Steve Harvey'),
+    ActivityBuilder.streaming(
+        'DF let\'s plays', 'https://www.twitch.tv/jeremaster104'),
+    ActivityBuilder.streaming(
+        'Minecraft magic', 'https://www.twitch.tv/jeremaster104'),
+    ActivityBuilder.streaming(
+        'free \$ giveaways', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'),
+    ActivityBuilder.streaming(
+        'cute cat videos', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'),
+    ActivityBuilder.game('League of Legends'),
+    ActivityBuilder.game('Legends of Runeterra'),
+    ActivityBuilder.game('Apex Legends'),
+    ActivityBuilder.game('Beat Saber'),
+    ActivityBuilder.game('TFT with HMusic'),
+    ActivityBuilder.game('with physics'),
+    ActivityBuilder.game('with my own code'),
+    ActivityBuilder.game('speedrun 1900s simulator'),
+    ActivityBuilder.game('during work hours'),
+    ActivityBuilder.listening('your conversations'),
+    ActivityBuilder.listening('your music'),
+    ActivityBuilder.listening('true crime podcasts'),
+    ActivityBuilder.listening('my loud neighbors'),
+  ];
+  void setRandomPresence() {
+    final activity = activities[_rng.nextInt(activities.length)];
+    bot.setPresence(PresenceBuilder.of(
+      status: UserStatus.online,
+      activity: activity,
+    ));
+  }
+
+  Future<void> handleHMessage(IMessage message) async {
+    await message.delete();
+    await message.channel.sendMessage(MessageBuilder.content(await getImage(
+      imageApi: imageApi,
+      customSearchEngine: customSearchEngine,
+      query: 'Letter H',
+    )));
+
+    final phrase = phraseGenerator
+        .generatePhrase()
+        .replaceAll('%name%', '<@${message.author.id}>');
+    await message.channel
+        .sendMessage(MessageBuilder.content('$phrase <@&421563234651471872>'));
+  }
 }
